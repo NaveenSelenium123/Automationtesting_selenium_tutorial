97457ebe6c12829159c12afe3ea40287418307fd
naveen@gmail.com <Naveen>
2022-05-09 08:17:54 +0530

    first one

---
+++ src/org/com/Simple.java
@@ -0,0 +1,28 @@
+package org.com;
+
+import java.io.File;
+import java.io.FileInputStream;
+//
+//import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Simple {
+//	File f =new File("D:\\Song\\TESTNG WORKSPACE\\DataDrivenFrameworkPractice\\Excel\\TestData.xlsx");
+//	FileInputStream fin =new FileInputStream(f);
+//	Workbook w =new XSSFWorkbook(fin);
+//	Sheet s = w.getSheet("Sheet1");
+//
+//	int physicalNumberOfRows = s.getPhysicalNumberOfRows();
+//	System.out.println("Row count" +physicalNumberOfRows);
+//	Row row1 = s.getRow(2);
+//	int physicalNumberOfCells = row1.getPhysicalNumberOfCells();
+//	System.out.println("cell count" +physicalNumberOfCells);
+//	System.out.println("============================");
+//	for (int i = 0; i < s.getPhysicalNumberOfRows(); i++) {
+//		Row row = s.getRow(i);
+//		for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
+//			Cell cell = row.getCell(j);
+}
